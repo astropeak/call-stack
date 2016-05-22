a199f1b5476c8ccfdc78a8c4bd674552007436d9
Astropeak Luo <astropeak@gmail.com>
2016-05-22 11:45:22 +0800

    Implement token.pl.

---
+++ test.pl
@@ -0,0 +1,37 @@
+package Aspk::Debug;
+use Filter::Simple;
+use File::Basename;
+use Exporter;
+
+use Scalar::Util qw(reftype);
+
+@ISA=qw(Exporter);
+@EXPORT_OK=qw(print_obj dbg_current_level);
+
+my $dbg_current_level= 4;
+
+my $objs = {};
+sub {
+    print "FFFFF";
+}
+sub print_call_stack {
+    my $i = 2;
+    my @rst;
+    my ($package, $filename, $line, $subroutine, $hasargs,
+        $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash) = caller($i++);
+
+    sub inline_sub{
+        print "I am a inline sub";
+    }
+
+    $filename = my_basename($filename, 2);
+    unshift @rst, [$filename, $line, $subroutine];
+    while ($subroutine ne "") {
+        ($package, $filename, $line, $subroutine, $hasargs,
+         $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash) = caller($i++);
+
+        $filename = my_basename($filename, 2);
+        unshift @rst, [$filename, $line, $subroutine];
+    }
+}
+
