25928b477aeb0659dd6531189d2c9fcaa460ce3c
Astropeak Luo <astropeak@163.com>
2016-05-23 13:19:46 +0800

    add pass file name as argument

--- main.pl
+++ main.pl
@@ -1,8 +1,12 @@
+#!/usr/bin/perl
+
 use Tokener;
 use ASTer;
 use Aspk::Debug;
 
-my $file = 'test.pl';
+my $file = @ARGV[0];
+die "Usage: perl main.pl FILE_NAME\n" unless defined $file;
+
 my $tk = Tokener->new($file);
 my $aster = ASTer->new($tk);
 $aster->build();
@@ -14,14 +18,13 @@ $aster->traverse({postfunc=>
                           my $data = $para->{data};
                           my $node = $para->{node};
                           if ($data->{type} eq 'subname') {
-                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>'print "Enter '.$data->{value}.'";'}}), 1);
-                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>'print "Exit '.$data->{value}.'";'}}), -1);
+                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>"\nprint 'Enter ".$data->{value}."'".'."\n";'}}), 1);
+                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>"print 'Exit ".$data->{value}."'".'."\n";'."\n"}}), -1);
                           }
                   }});
 
 # $aster->display();
 open my $fh, '>', "add_trace_$file" or die "Can't open file";
-
 $aster->traverse({prefunc=>
                       sub{
                           my $para = shift;
