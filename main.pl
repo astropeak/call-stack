aef5c89af96c1753beb89554f4996c73f6e9b0b5
Astropeak Luo <astropeak@gmail.com>
2016-05-23 20:18:53 +0800

    add file name and row number

--- main.pl
+++ main.pl
@@ -12,22 +12,39 @@ my $tk = Tokener->new($file);
 my $aster = ASTer->new($tk);
 $aster->build();
 
+# calculate row number
+my $total_row=1;
+$aster->traverse({prefunc=>
+                      sub{
+                          my $para = shift;
+                          my $data = $para->{data};
+                          my $cnt = @{[$data->{value} =~ /\n/g]};
+                          $data->{row}=$total_row;
+                          $total_row+=$cnt;
+                  }
+                 });
+
 # add enter and exit trace
 $aster->traverse({postfunc=>
                       sub{
                           my $para = shift;
                           my $data = $para->{data};
                           my $node = $para->{node};
+                          sub header {
+                              my ($file, $row) = @_;
+                              return "[".basename($file).":$row]";
+                          }
+
                           if ($data->{type} eq 'subname') {
-                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>"\nprint 'Enter ".$data->{value}."'".'."\n";'}}), 1);
-                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>"print 'Exit ".$data->{value}."'".'."\n";'."\n"}}), -1);
+                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>"\nprint '".header($file, $data->{row})." Enter ".$data->{value}."'".'."\n";'}}), 1);
+                              $node->add_child(Aspk::Tree->new({data=>{type=>'other',value=>"print '".header($file, $node->prop(children)->[-1]->prop(data)->{row})." Exit ".$data->{value}."'".'."\n";'."\n"}}), -1);
 
                               # add before all return
                               my @new_children;
                               foreach (@{$node->prop(children)}) {
                                   my $d = $_->prop(data);
                                   if ($d->{type} eq 'literal' && $d->{value} eq 'return') {
-                                      push @new_children, Aspk::Tree->new({data=>{type=>'other',value=>"print 'Exit ".$data->{value}."'".'."\n";'."\n"}});
+                                      push @new_children, Aspk::Tree->new({data=>{type=>'other',value=>"print '".header($file, $_->prop(data)->{row})." Exit ".$data->{value}."'".'."\n";'."\n"}});
                                   }
 
                                   push @new_children, $_;
