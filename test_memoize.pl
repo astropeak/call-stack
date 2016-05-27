7826e2a53cf3108514d27afe48ce35a6cad2d5f9
Astropeak Luo <astropeak@gmail.com>
2016-05-27 16:08:33 +0800

    add a test file

---
+++ test_memoize.pl
@@ -0,0 +1,17 @@
+use MemoizeAT;
+use AddTrace;
+
+memoize fib;
+sub fib {
+    my $d = shift;
+    print "Enter fib, $d\n";
+    if ($d== 1 || $d==2){
+        return 1;
+    } else {
+        return fib($d-1)+fib($d-2);
+    }
+}
+$a=fib(4);
+print "fib(4) result:$a\n";
+$a=fib(4);
+print "fib(4) result:$a\n";
